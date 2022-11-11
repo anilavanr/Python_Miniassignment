4576d21e4bc696701ec7dd7b6f253caac5618da6
anilavanr <anilavanr@deloitte.com>
2022-11-11 18:35:35 +0530

    done

---
+++ Assignment5/Decorator.py
@@ -0,0 +1,5 @@
+def multiply(num1, num2):
+    print(num1 * num2)
+
+
+print(multiply(5, 5))
---
+++ Assignment5/fibonacciGenerator.py
@@ -0,0 +1,13 @@
+def fibonaccigenerator():
+    n2 = 1
+    n3 = 1
+    while True:
+        n1 = n2
+        n2 = n3
+        n3 = n1 + n2
+        yield n1
+
+
+a = fibonaccigenerator()
+for i in range(20):
+    print(next(a))
